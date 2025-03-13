c2b47efcaefaf29421c45de1914bcf78e0a8168c
deveshbis <deveshbiswas.cse@gmail.com>
2025-03-13 15:31:28 +0600

    one-stop-solution change english to bangali language

--- lang/en/welcome.php
+++ lang/en/welcome.php
@@ -3,4 +3,12 @@
 return [
     'title' => 'Welcome to BDFunnelBuilder',
 
+    'one_stop_solution' => [
+        'title' => 'Who is BD Funnel Builder For?',
+        'sub-title' => 'One-stop solution for all digital entrepreneurs',
+        'cardTitle1' => 'E-Commerce Entrepreneur',
+        'cardTitle2' => 'E-Commerce Entrepreneur',
+        'cardTitle3' => 'Digital Business Owner',
+    ]
+
 ];
--- resources/views/components/one-stop-solution.blade.php
+++ resources/views/components/one-stop-solution.blade.php
@@ -10,26 +10,26 @@
 
 <body>
     <div>
-        <h4 class="text-[24px] font-[300] text-center">One-stop solution for all digital entrepreneurs</h4>
-        <h2 class="text-[60px] font-[800] text-gray-800 text-center">Who is BD Funnel Builder For?</h2>
+        <h4 class="text-[24px] font-[300] text-center">{{__('welcome.one_stop_solution.sub-title')}}</h4>
+        <h2 class="text-[60px] font-[800] text-gray-800 text-center">{{__('welcome.one_stop_solution.title')}}</h2>
         <div class="p-1 flex flex-wrap items-center justify-center">
             <div class="flex-shrink-0 m-6 relative overflow-hidden bg-[#FEF5DB] rounded-lg w-[383px] h-[355px] shadow-lg">
                 <div class="relative pt-10 px-10 flex items-center justify-center">
                     <img class="relative w-[310px] h-[204px]" src="/images/one-stop-solution/one-stop-solution1.png" alt="">
                 </div>
-                <h3 class="pt-10 font-[500] text-[26px] text-center">E-Commerce Entrepreneur</h3>
+                <h3 class="pt-10 font-[500] text-[26px] text-center">{{__('welcome.one_stop_solution.cardTitle1')}}</h3>
             </div>
             <div class="flex-shrink-0 m-6 relative overflow-hidden bg-[#DBEBFF] rounded-lg w-[383px] h-[355px] shadow-lg">
                 <div class="relative pt-10 px-10 flex items-center justify-center">
                     <img class="relative w-[310px] h-[204px]" src="/images/one-stop-solution/one-stop-solution2.png" alt="">
                 </div>
-                <h3 class="pt-10 font-[500] text-[26px] text-center">E-Commerce Entrepreneur</h3>
+                <h3 class="pt-10 font-[500] text-[26px] text-center">{{__('welcome.one_stop_solution.cardTitle2')}}</h3>
             </div>
             <div class="flex-shrink-0 m-6 relative overflow-hidden bg-[#FEF5DB] rounded-lg w-[383px] h-[355px] shadow-lg">
                 <div class="relative pt-10 px-10 flex items-center justify-center">
                     <img class="relative w-[310px] h-[204px]" src="/images/one-stop-solution/one-stop-solution3.png" alt="">
                 </div>
-                <h3 class="pt-10 font-[500] text-[26px] text-center">Digital Business Owner</h3>
+                <h3 class="pt-10 font-[500] text-[26px] text-center">{{__('welcome.one_stop_solution.cardTitle3')}}</h3>
             </div>
 
         </div>
