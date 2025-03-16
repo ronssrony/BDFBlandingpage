e939a1b1d42118a9a8c08df52927460dd0c1f17d
ronssrony <ronssrony@gmail.com>
2025-03-16 15:29:28 +0600

    fix the serials of content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,16 @@
     <div class="nav mb-28" >
         @include('components.navbar')
     </div>
-    @include('components.smarter-business')
-    @include('components.unlimited-templates')
 
+    @include('components.smarter-business')
+    @include('components.boost-business')
+    @include('components.pricing-mission-vission')
+    @include('components.unlimited-templates')
     @include('components.essential-business')
     @include('components.one-stop-solution')
     @include('components.growth-story')
     @include('components.team-behind')
+    @include('components.faq')
     @include('components.footer')
 </div>
 
