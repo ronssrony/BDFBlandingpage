2ee8369bed2a3f6aabf0439a9fbd0144d6dd4c82
ronssrony <ronssrony@gmail.com>
2025-03-16 16:05:18 +0600

    tt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <div class="nav mb-28" >
         @include('components.navbar')
     </div>
-
+    @include('components.brands-marquee')
     @include('components.smarter-business')
     @include('components.boost-business')
     @include('components.one-stop-solution')
@@ -13,6 +13,7 @@
     @include('components.growth-story')
     @include('components.team-behind')
     @include('components.faq')
+    @include('components.footer')
 
 </div>
 
