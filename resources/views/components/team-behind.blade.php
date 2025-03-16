b04aad9e4de9271c35ee74a064b9d1ae52b22cd4
ronssrony <ronssrony@gmail.com>
2025-03-16 15:27:37 +0600

    fix dev keep the align code

--- resources/views/components/team-behind.blade.php
+++ resources/views/components/team-behind.blade.php
@@ -1,6 +1,6 @@
 
 <x-layout>
-    <div class="dark:bg-gray-800 mt-4 py-6 sm:py-8 lg:py-12">
+    <div class=" mt-4 py-6 sm:py-8 lg:py-12">
         <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
             <div class="flex justify-center items-center">
                 <h3 class="max-w-screen-sm lg:text-[24px] md:text-[20px] text-[18px] font-[300] text-gray-500 dark:text-gray-300">
