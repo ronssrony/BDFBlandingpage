8e8dfca77739dfd47199fb35b8462a8cac2c0e6e
Rokiya Ripa <rokaiyaripa4029@gmail.com>
2025-03-17 10:08:23 +0600

    confilct solve

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,24 +3,20 @@
     <div class="nav mb-28" >
         @include('components.navbar')
     </div>
-
-    @include('components.hero')
+    @include('components.brands-marquee')
+    @include('components.smarter-business')
+    @include('components.boost-business')
+    @include('components.one-stop-solution')
+    @include('components.essential-business')
+    @include('components.unlimited-templates')
     @include('components.pricing-mission-vission')
     @include('components.mission-vission')
     @include('components.growth-story')
-
-    @include('components.smarter-business')
-    @include('components.boost-business')
-    @include('components.essential-business')
-    @include('components.unlimited-templates')
-    @include('components.one-stop-solution')
     @include('components.team-behind')
     @include('components.faq')
     @include('components.footer')
+
 </div>
-
-
-
 
 
 
