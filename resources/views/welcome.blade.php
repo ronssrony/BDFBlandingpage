bfbd38dc0dcb3988a0ef08f5a841f8d7750f3e82
Shafa Islam <shafaislam050@gmail.com>
2025-03-17 12:51:19 +0600

    layout removed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,24 @@
-
-<<<<<<< HEAD
-@include('components.hero')
-@include('components.growth-story')
-=======
-<div>
-    <div class="nav mb-28" >
+<x-layout>
+    <div class="nav mb-28">
         @include('components.navbar')
+
     </div>
-
-
+    <div>
+    @include('components.hero')
+    @include('components.brands-marquee')
     @include('components.smarter-business')
     @include('components.boost-business')
+    @include('components.one-stop-solution')
     @include('components.essential-business')
     @include('components.unlimited-templates')
-    @include('components.one-stop-solution')
+    @include('components.mission-vision')
+    @include('components.growth-story')
     @include('components.team-behind')
     @include('components.faq')
+    @include('components.footer')
+    @include('components.testimonials')
+    @include('components.terms-condition')
+    @include('components.privacy-policy')
 
 </div>
-
-
-
-
-
-
+</x-layout>
