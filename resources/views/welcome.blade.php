0eb53b33cb93ac7cd6e5f4450ee520f26a6aa11a
ronssrony <ronssrony@gmail.com>
2025-03-17 12:22:40 +0600

    fix some issue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,26 +2,23 @@
     <div class="nav mb-28">
         @include('components.navbar')
 
-
+    </div>
+    <div>
     @include('components.hero')
-    @include('components.growth-story')
-
     @include('components.brands-marquee')
     @include('components.smarter-business')
     @include('components.boost-business')
     @include('components.one-stop-solution')
     @include('components.essential-business')
     @include('components.unlimited-templates')
-    @include('components.pricing-mission-vission')
+    @include('components.mission-vision')
     @include('components.growth-story')
     @include('components.team-behind')
     @include('components.faq')
     @include('components.footer')
     @include('components.testimonials')
-</x-layout>
-
-
     @include('components.terms-condition')
     @include('components.privacy-policy')
 
 </div>
+</x-layout>
